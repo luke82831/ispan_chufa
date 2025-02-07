b5a1654727bbdc851adf78fb45cba623a7aa50b1
tszmem <tszmen1104@gmail.com>
2025-02-07 14:30:11 +0800

    fixed a little

--- proj-chufa/src/main/java/com/ispan/chufa/domain/PlaceBean.java
+++ proj-chufa/src/main/java/com/ispan/chufa/domain/PlaceBean.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
